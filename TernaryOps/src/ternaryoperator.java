f458ea72edd0c1c444c1cc7643a7e08245ead207
= <=>
2025-06-27 12:29:10 +0530

    completed Conditions chapter

---
+++ TernaryOps/src/ternaryoperator.java
@@ -0,0 +1,10 @@
+public class ternaryoperator {
+
+    public static void main(String[] args) {
+        int i= 6;
+        boolean isEven = (i%2==0 ? true : false );
+        System.out.println(isEven);
+
+    }
+
+}
